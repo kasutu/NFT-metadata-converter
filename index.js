6c7d27f838bf1902e227e5043ef0c60abd732ea5
PZYCO20 <64959978+PZYCO20@users.noreply.github.com>
2022-01-28 13:29:27 +0800

    made a beautiful cli

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
+#!/usr/bin/env node
+
 // init
-const fs = require('fs');
-const colors = require('colors');
+import { readdir, readFile } from 'fs';
+import chalk from 'chalk';
+// import { meta } from './kasutu_modules/metaTemplates';
 
 // directories
 const binDir = './bin';
 const outputDir = './output';
 
 // global variables
+const sleep = (ms = 1500) => new Promise((r) => setTimeout(r, ms));
 let startPace;
 const metadataArr = [];
 
 // global functions
+
 function print(log) {
 	// logs in console
-	console.log('-------------------------------------------------------------');
-	console.log(log);
-	// console.log('-------------------------------------------------------------');
+	console.log('>>', chalk.bgBlue(log));
 }
 
 function printErr(error) {
 	// error log handler
-	console.log('#############################################################');
-	console.log({ error });
-	// console.log('#############################################################');
+	console.log('>>', chalk.red(error));
+}
+
+function logger(log) {
+	// logs in console
+	console.log('>>', chalk.bgGreen(log));
+}
+
+function report(log) {
+	// logs in console
+	console.log('>>', log);
 }
 
 async function countFiles(dir) {
 	// working total file counter
 	return new Promise((resolve, reject) =>
-		fs.readdir(dir, (err, files) =>
+		readdir(dir, (err, files) =>
 			err ? reject(printErr(err)) : resolve(files.length)
 		)
 	);
@@ -36,7 +47,7 @@ async function countFiles(dir) {
 
 async function makeSequence(length) {
 	// makes a code firing sequence
-	print('sequencing...'.blue);
+	print('sequencing...');
 	const sequence = [];
 
 	// make an array of numbers 1 to length
@@ -44,28 +55,25 @@ async function makeSequence(length) {
 		sequence.push(i + 1);
 	}
 
-	print('done sequencing...'.yellow);
+	print('done sequencing...');
 	return sequence;
 }
 
 async function checkFolder(dir) {
 	// check folder for available files
-	print('checking folder for files...'.blue);
 
 	return new Promise((resolve, reject) =>
-		fs.readdir(dir, (err, files) =>
-			err ? reject(printErr(err)) : resolve(files)
-		)
+		readdir(dir, (err, files) => (err ? reject(printErr(err)) : resolve(files)))
 	);
 }
 
 async function getData(dir, fileName) {
 	// grabs the data inside a json file
-	console.log(`grabbing data from: [  ${fileName}  ]`.blue);
+	// logger(`grabbing data from: [  ${fileName}  ]`);
 
 	return new Promise((resolve, reject) => {
 		// check and save data
-		fs.readFile(`${dir}/${fileName}`, 'utf-8', (err, jsonStr) => {
+		readFile(`${dir}/${fileName}`, 'utf-8', (err, jsonStr) => {
 			err ? reject(printErr(err)) : resolve(JSON.parse(jsonStr));
 		});
 	});
@@ -75,24 +83,24 @@ async function pushMeta(files, dir, arr) {
 	// read each files and push it to an array
 	for (const file of files) {
 		const meta = await getData(dir, file);
-		arr.push(meta.name);
-		console.log(`pushed: [  ${file}  ]`.yellow);
+		arr.push(meta);
+		logger(`pushing: [  ${file}  ]`);
 	}
 }
 
 const initialize = async () => {
 	// executes functions in order
-	print('INITIALIZING...'.yellow);
+	print('INITIALIZING...');
+	await sleep(); // sleeps for 1.5 sec
 
 	// checks folder for available files
 	const files = await checkFolder(binDir);
-	console.log('files:', files);
-	
+
 	// push all data to array
 	await pushMeta(files, binDir, metadataArr);
 
-	print(metadataArr);
-	print('DONE INIT...'.yellow);
+	report(`converted: ${metadataArr.length} files`);
+	print('DONE INIT...');
 };
 
 // ----------Init----------//
